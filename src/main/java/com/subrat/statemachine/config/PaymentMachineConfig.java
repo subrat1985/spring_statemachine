7e5f973a5f23bdeea2f654bbba01397ec86a9582
subrat1985 <subrat.pintu1@gmail.com>
2021-06-28 09:58:43 +0530

    action added

--- src/main/java/com/subrat/statemachine/config/PaymentMachineConfig.java
+++ src/main/java/com/subrat/statemachine/config/PaymentMachineConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
@@ -22,28 +23,43 @@ public class PaymentMachineConfig extends StateMachineConfigurerAdapter<PaymentS
     public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
         states.withStates()
         .initial(PaymentState.NEW)
-        .state(PaymentState.VALIDATE,stateContext -> stateContext.getStateMachine().sendEvent(PaymentEvent.VALIDATED))
-        .state(PaymentState.DEBIT,stateContext -> stateContext.getStateMachine().sendEvent(PaymentEvent.DEBITED))
-        .state(PaymentState.CREDIT,stateContext -> stateContext.getStateMachine().sendEvent(PaymentEvent.CREDITED))
-        .state(PaymentState.DEBIT_FAIL,stateContext -> stateContext.getStateMachine().sendEvent(PaymentEvent.DEBIT_DECLINED))
-        .state(PaymentState.CREDIT_FAIL,stateContext -> stateContext.getStateMachine().sendEvent(PaymentEvent.CREDIT_DECLINED))
+        .state(PaymentState.VALIDATE)
+        .state(PaymentState.DEBIT)
+        .state(PaymentState.CREDIT)
+        .state(PaymentState.DEBIT_FAIL)
+        .state(PaymentState.CREDIT_FAIL)
         .end(PaymentState.COMPLETE);
     }
 
     @Override
     public void configure(StateMachineTransitionConfigurer<PaymentState, PaymentEvent> transitions) throws Exception {
-        transitions.withExternal().source(PaymentState.NEW).target(PaymentState.VALIDATE).event(PaymentEvent.NEW_PAYMENT)
+        transitions.withExternal().source(PaymentState.NEW).target(PaymentState.VALIDATE).event(PaymentEvent.NEW_PAYMENT).action(action1())
         .and()
-        .withExternal().source(PaymentState.VALIDATE).target(PaymentState.DEBIT).event(PaymentEvent.DEBITED)
+        .withExternal().source(PaymentState.VALIDATE).target(PaymentState.DEBIT).event(PaymentEvent.DEBITED).action(action2())
         .and()
-        .withExternal().source(PaymentState.DEBIT).target(PaymentState.CREDIT).event(PaymentEvent.CREDITED)
+        .withExternal().source(PaymentState.DEBIT).target(PaymentState.CREDIT).event(PaymentEvent.CREDITED).action(action3())
         .and()
-        .withExternal().source(PaymentState.CREDIT).target(PaymentState.COMPLETE).event(PaymentEvent.COMPLETED)
+        .withExternal().source(PaymentState.CREDIT).target(PaymentState.COMPLETE).event(PaymentEvent.COMPLETED).action(action4())
         .and()
         .withExternal().source(PaymentState.DEBIT).target(PaymentState.DEBIT_FAIL).event(PaymentEvent.DEBIT_DECLINED)
         .and()
         .withExternal().source(PaymentState.CREDIT).target(PaymentState.CREDIT_FAIL).event(PaymentEvent.CREDIT_DECLINED);
     }
+
+
+    private Action<PaymentState, PaymentEvent> action1() {
+        return stateContext->stateContext.getStateMachine().sendEvent(PaymentEvent.NEW_PAYMENT);
+    }
+    private Action<PaymentState, PaymentEvent> action2() {
+        return stateContext->stateContext.getStateMachine().sendEvent(PaymentEvent.DEBITED);
+    }
+    private Action<PaymentState, PaymentEvent> action3() {
+        return stateContext->stateContext.getStateMachine().sendEvent(PaymentEvent.CREDITED);
+    }
+    private Action<PaymentState, PaymentEvent> action4() {
+        return stateContext->stateContext.getStateMachine().sendEvent(PaymentEvent.COMPLETED);
+    }
+
 
     @Override
     public void configure(StateMachineConfigurationConfigurer<PaymentState, PaymentEvent> config) throws Exception {
